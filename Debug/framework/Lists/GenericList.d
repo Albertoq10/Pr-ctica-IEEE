b87aa3278f1ba013f6d86f4c7728d0526a371bd9
Diego Delgado <ie730460@iteso.mx>
2024-10-29 23:30:17 -0600

    sends to the right address

---
+++ Debug/framework/Lists/GenericList.d
@@ -0,0 +1,12 @@
+framework/Lists/GenericList.o framework/Lists/GenericList.d: \
+ ../framework/Lists/GenericList.c \
+ C:/Users/diego/Downloads/frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm/source/app_preinclude.h \
+ ../framework/Lists/GenericList.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\common/EmbeddedTypes.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\OSAbstraction\Interface/fsl_os_abstraction.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\OSAbstraction\Interface/fsl_os_abstraction_config.h
+C:/Users/diego/Downloads/frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm/source/app_preinclude.h:
+../framework/Lists/GenericList.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\common/EmbeddedTypes.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\OSAbstraction\Interface/fsl_os_abstraction.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\OSAbstraction\Interface/fsl_os_abstraction_config.h:
---
+++ Debug/framework/NVM/Source/NV_FsciCommands.d
@@ -0,0 +1,17 @@
+framework/NVM/Source/NV_FsciCommands.o \
+ framework/NVM/Source/NV_FsciCommands.d: \
+ ../framework/NVM/Source/NV_FsciCommands.c \
+ C:/Users/diego/Downloads/frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm/source/app_preinclude.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\common/EmbeddedTypes.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\Messaging\Interface/Messaging.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\Lists/GenericList.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\MemManager\Interface/MemManager.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\FunctionLib/FunctionLib.h \
+ C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\NVM\Interface/NVM_Interface.h
+C:/Users/diego/Downloads/frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm/source/app_preinclude.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\common/EmbeddedTypes.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\Messaging\Interface/Messaging.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\Lists/GenericList.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\MemManager\Interface/MemManager.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\FunctionLib/FunctionLib.h:
+C:\Users\diego\Downloads\frdmkw41z_wireless_examples_ieee_802_15_4_mwa_end_device_bm\framework\NVM\Interface/NVM_Interface.h:
